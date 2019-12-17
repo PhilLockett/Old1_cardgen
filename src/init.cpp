e8192a9382c432a544a5e21f418edf54ed35f91f
Phil Lockett <phillockett65@gmail.com>
2019-12-17 17:43:21 -0500

    Added copyright and updated README.

--- src/init.cpp
+++ src/init.cpp
@@ -1,3 +1,25 @@
+/*  cardgen - a playing card image generator.
+
+    Copyright 2019 Philip Lockett.
+
+    This file is part of cardgen.
+
+    cardgen is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    cardgen is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with cardgen.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+///////////////////////////////////////////////////////////////////////////////
+//
 // init.cpp: Command line parameter initialisation for the card generator.
 //
 ///////////////////////////////////////////////////////////////////////////////
